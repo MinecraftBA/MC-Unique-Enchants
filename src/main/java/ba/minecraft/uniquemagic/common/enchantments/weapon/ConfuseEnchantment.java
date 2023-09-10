bbc604b135df82d07a9b87cb489d58bfea719f35
AzureAD\DilanTuzovic <dilan.tuzovic@appimpact.ba>
2023-09-10 18:38:54 +0200

    Added two new enchants

--- src/main/java/ba/minecraft/uniquemagic/common/enchantments/weapon/ConfuseEnchantment.java
+++ src/main/java/ba/minecraft/uniquemagic/common/enchantments/weapon/ConfuseEnchantment.java
@@ -47,6 +47,6 @@ public class ConfuseEnchantment extends ApplyEffectEnchantment {
 	
 	@Override
 	protected String getBaseName() {
-		return "Weaken";
+		return "Confuse";
 	}
 }
--- src/main/java/ba/minecraft/uniquemagic/common/enchantments/weapon/SlowEnchantment.java
+++ src/main/java/ba/minecraft/uniquemagic/common/enchantments/weapon/SlowEnchantment.java
@@ -47,6 +47,6 @@ public class SlowEnchantment extends ApplyEffectEnchantment {
 	
 	@Override
 	protected String getBaseName() {
-		return "Weaken";
+		return "Slow";
 	}
 }
