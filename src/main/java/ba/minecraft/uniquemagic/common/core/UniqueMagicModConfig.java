37640c7efc7cc2808114b3ec5783be72367e3a1b
AzureAD\DilanTuzovic <dilan.tuzovic@appimpact.ba>
2024-01-13 22:58:29 +0100

    added config for poison

--- src/main/java/ba/minecraft/uniquemagic/common/core/UniqueMagicModConfig.java
+++ src/main/java/ba/minecraft/uniquemagic/common/core/UniqueMagicModConfig.java
@@ -17,11 +17,17 @@ public final class UniqueMagicModConfig {
     private static final ForgeConfigSpec.IntValue WITHERING_BASE_CHANCE_CONFIG;
     private static final ForgeConfigSpec.IntValue WITHERING_BASE_DURATION_CONFIG;
     
+    private static final ForgeConfigSpec.IntValue POISON_BASE_CHANCE_CONFIG;
+    private static final ForgeConfigSpec.IntValue POISON_BASE_DURATION_CONFIG;
+    
     public static int EXECUTE_BASE_CHANCE;
     public static int LIFE_STEAL_BASE_RATIO;
     
     public static int WITHERING_BASE_CHANCE;
     public static int WITHERING_BASE_DURATION;
+    
+    public static int POISON_BASE_CHANCE;
+    public static int POISON_BASE_DURATION;
     
     static {
     	BUILDER.push("Configs for Unique Magic Mod");
@@ -37,6 +43,12 @@ public final class UniqueMagicModConfig {
 
     	WITHERING_BASE_DURATION_CONFIG = BUILDER.comment("Base duration for Withering enchantment in seconds.")
     	        .defineInRange("Withering base duration", 3, 1, 60);
+    	
+    	POISON_BASE_CHANCE_CONFIG = BUILDER.comment("Base chance for Poison enchantment to trigger.")
+    	        .defineInRange("Poison base chance", 5, 1, 20);
+
+    	POISON_BASE_DURATION_CONFIG = BUILDER.comment("Base duration for Poison enchantment in seconds.")
+    	        .defineInRange("Poison base duration", 3, 1, 60);
 
     	BUILDER.pop();
     	
@@ -51,5 +63,8 @@ public final class UniqueMagicModConfig {
     	
     	WITHERING_BASE_CHANCE = WITHERING_BASE_CHANCE_CONFIG.get();
     	WITHERING_BASE_DURATION = WITHERING_BASE_DURATION_CONFIG.get();
+    	
+    	POISON_BASE_CHANCE = POISON_BASE_CHANCE_CONFIG.get();
+    	POISON_BASE_DURATION = POISON_BASE_DURATION_CONFIG.get();
 	}
 }
