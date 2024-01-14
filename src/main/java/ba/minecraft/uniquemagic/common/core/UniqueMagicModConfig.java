208686880c0297223794a108502ddb59ee964618
AzureAD\DilanTuzovic <dilan.tuzovic@appimpact.ba>
2024-01-14 14:23:14 +0100

    config

--- src/main/java/ba/minecraft/uniquemagic/common/core/UniqueMagicModConfig.java
+++ src/main/java/ba/minecraft/uniquemagic/common/core/UniqueMagicModConfig.java
@@ -20,6 +20,36 @@ public final class UniqueMagicModConfig {
     private static final ForgeConfigSpec.IntValue POISON_BASE_CHANCE_CONFIG;
     private static final ForgeConfigSpec.IntValue POISON_BASE_DURATION_CONFIG;
     
+    private static final ForgeConfigSpec.IntValue BLIND_BASE_CHANCE_CONFIG;
+    private static final ForgeConfigSpec.IntValue BLIND_BASE_DURATION_CONFIG;
+    
+    private static final ForgeConfigSpec.IntValue CONFUSE_BASE_CHANCE_CONFIG;
+    private static final ForgeConfigSpec.IntValue CONFUSE_BASE_DURATION_CONFIG;
+    
+    private static final ForgeConfigSpec.IntValue CURSE_BASE_CHANCE_CONFIG;
+    private static final ForgeConfigSpec.IntValue CURSE_BASE_DURATION_CONFIG;
+    
+    private static final ForgeConfigSpec.IntValue HARM_BASE_CHANCE_CONFIG;
+    private static final ForgeConfigSpec.IntValue HARM_BASE_DURATION_CONFIG;
+    
+    private static final ForgeConfigSpec.IntValue LEVITATE_BASE_CHANCE_CONFIG;
+    private static final ForgeConfigSpec.IntValue LEVITATE_BASE_DURATION_CONFIG;
+    
+    private static final ForgeConfigSpec.IntValue LIGHTNING_STRIKER_BASE_CHANCE_CONFIG;
+    private static final ForgeConfigSpec.IntValue LIGHTNING_STRIKER_BASE_DURATION_CONFIG;
+    
+    private static final ForgeConfigSpec.IntValue SLOW_BASE_CHANCE_CONFIG;
+    private static final ForgeConfigSpec.IntValue SLOW_BASE_DURATION_CONFIG;
+    
+    private static final ForgeConfigSpec.IntValue STARVE_BASE_CHANCE_CONFIG;
+    private static final ForgeConfigSpec.IntValue STARVE_BASE_DURATION_CONFIG;
+    
+    private static final ForgeConfigSpec.IntValue TEMPO_THEFT_BASE_CHANCE_CONFIG;
+    private static final ForgeConfigSpec.IntValue TEMPO_THEFT_BASE_DURATION_CONFIG;
+    
+    private static final ForgeConfigSpec.IntValue WEAKEN_BASE_CHANCE_CONFIG;
+    private static final ForgeConfigSpec.IntValue WEAKEN_BASE_DURATION_CONFIG;
+    
     public static int EXECUTE_BASE_CHANCE;
     public static int LIFE_STEAL_BASE_RATIO;
     
@@ -29,6 +59,35 @@ public final class UniqueMagicModConfig {
     public static int POISON_BASE_CHANCE;
     public static int POISON_BASE_DURATION;
     
+    public static int BLIND_BASE_CHANCE;
+    public static int BLIND_BASE_DURATION;
+    
+    public static int CONFUSE_BASE_CHANCE;
+    public static int CONFUSE_BASE_DURATION;
+    
+    public static int CURSE_BASE_CHANCE;
+    public static int CURSE_BASE_DURATION;
+    
+    public static int HARM_BASE_CHANCE;
+    public static int HARM_BASE_DURATION;
+    
+    public static int LEVITATE_BASE_CHANCE;
+    public static int LEVITATE_BASE_DURATION;
+    
+    public static int LIGHTNING_STRIKER_BASE_CHANCE;
+    public static int LIGHTNING_STRIKER_BASE_DURATION;
+    
+    public static int SLOW_BASE_CHANCE;
+    public static int SLOW_BASE_DURATION;
+    
+    public static int STARVE_BASE_CHANCE;
+    public static int STARVE_BASE_DURATION;
+    
+    public static int TEMPO_THEFT_BASE_CHANCE;
+    public static int TEMPO_THEFT_BASE_DURATION;
+    
+    public static int WEAKEN_BASE_CHANCE;
+    public static int WEAKEN_BASE_DURATION;
     static {
     	BUILDER.push("Configs for Unique Magic Mod");
 
@@ -49,6 +108,66 @@ public final class UniqueMagicModConfig {
 
     	POISON_BASE_DURATION_CONFIG = BUILDER.comment("Base duration for Poison enchantment in seconds.")
     	        .defineInRange("Poison base duration", 3, 1, 60);
+    	
+    	BLIND_BASE_CHANCE_CONFIG = BUILDER.comment("Base chance for Blind enchantment to trigger.")
+    	        .defineInRange("Blind base chance", 5, 1, 20);
+
+    	BLIND_BASE_DURATION_CONFIG = BUILDER.comment("Base duration for Blind enchantment in seconds.")
+    	        .defineInRange("Blind base duration", 3, 1, 60);
+    	
+    	CONFUSE_BASE_CHANCE_CONFIG = BUILDER.comment("Base chance for Confuse enchantment to trigger.")
+    	        .defineInRange("Confuse base chance", 5, 1, 20);
+
+    	CONFUSE_BASE_DURATION_CONFIG = BUILDER.comment("Base duration for Confuse enchantment in seconds.")
+    	        .defineInRange("Confuse base duration", 3, 1, 60);
+    	
+    	CURSE_BASE_CHANCE_CONFIG = BUILDER.comment("Base chance for Curse Of Slowness enchantment to trigger.")
+    	        .defineInRange("Curse Of Slowness base chance", 5, 1, 20);
+
+    	CURSE_BASE_DURATION_CONFIG = BUILDER.comment("Base duration for Curse Of Slowness enchantment in seconds.")
+    	        .defineInRange("Curse Of Slowness base duration", 3, 1, 60);
+    	
+    	HARM_BASE_CHANCE_CONFIG = BUILDER.comment("Base chance for Harm enchantment to trigger.")
+    	        .defineInRange("Harm base chance", 5, 1, 20);
+
+    	HARM_BASE_DURATION_CONFIG = BUILDER.comment("Base duration for Harm enchantment in seconds.")
+    	        .defineInRange("Harm base duration", 3, 1, 60);
+    	
+    	LEVITATE_BASE_CHANCE_CONFIG = BUILDER.comment("Base chance for Harm enchantment to trigger.")
+    	        .defineInRange("Harm base chance", 5, 1, 20);
+
+    	LEVITATE_BASE_DURATION_CONFIG = BUILDER.comment("Base duration for Levitate enchantment in seconds.")
+    	        .defineInRange("Levitate base duration", 3, 1, 60);
+    	
+    	LIGHTNING_STRIKER_BASE_CHANCE_CONFIG = BUILDER.comment("Base chance for Lightning Striker enchantment to trigger.")
+    	        .defineInRange("Lightning Striker base chance", 5, 1, 20);
+
+    	LIGHTNING_STRIKER_BASE_DURATION_CONFIG = BUILDER.comment("Base duration for Lightning Striker enchantment in seconds.")
+    	        .defineInRange("Lightning Striker base duration", 3, 1, 60);
+    	
+    	SLOW_BASE_CHANCE_CONFIG = BUILDER.comment("Base chance for Slow enchantment to trigger.")
+    	        .defineInRange("Slow base chance", 5, 1, 20);
+
+    	SLOW_BASE_DURATION_CONFIG = BUILDER.comment("Base duration for Slow enchantment in seconds.")
+    	        .defineInRange("Slow base duration", 3, 1, 60);
+    	
+    	STARVE_BASE_CHANCE_CONFIG = BUILDER.comment("Base chance for Starve enchantment to trigger.")
+    	        .defineInRange("Starve base chance", 5, 1, 20);
+
+    	STARVE_BASE_DURATION_CONFIG = BUILDER.comment("Base duration for Starve enchantment in seconds.")
+    	        .defineInRange("Starve base duration", 3, 1, 60);
+    	
+    	TEMPO_THEFT_BASE_CHANCE_CONFIG = BUILDER.comment("Base chance for Tempo Theft enchantment to trigger.")
+    	        .defineInRange("Tempo Theft base chance", 5, 1, 20);
+
+    	TEMPO_THEFT_BASE_DURATION_CONFIG = BUILDER.comment("Base duration for Tempo Theft enchantment in seconds.")
+    	        .defineInRange("Tempo Theft base duration", 3, 1, 60);
+    	
+    	WEAKEN_BASE_CHANCE_CONFIG = BUILDER.comment("Base chance for Weaken enchantment to trigger.")
+    	        .defineInRange("Tempo Theft base chance", 5, 1, 20);
+
+    	WEAKEN_BASE_DURATION_CONFIG = BUILDER.comment("Base duration for Tempo Theft enchantment in seconds.")
+    	        .defineInRange("Tempo Theft base duration", 3, 1, 60);
 
     	BUILDER.pop();
     	
@@ -66,5 +185,38 @@ public final class UniqueMagicModConfig {
     	
     	POISON_BASE_CHANCE = POISON_BASE_CHANCE_CONFIG.get();
     	POISON_BASE_DURATION = POISON_BASE_DURATION_CONFIG.get();
+    	
+    	BLIND_BASE_CHANCE = BLIND_BASE_CHANCE_CONFIG.get();
+    	BLIND_BASE_DURATION = BLIND_BASE_DURATION_CONFIG.get();
+    	
+    	CONFUSE_BASE_CHANCE = CONFUSE_BASE_CHANCE_CONFIG.get();
+    	CONFUSE_BASE_DURATION = CONFUSE_BASE_DURATION_CONFIG.get();
+    	
+    	CURSE_BASE_CHANCE = CURSE_BASE_CHANCE_CONFIG.get();
+    	CURSE_BASE_DURATION = CURSE_BASE_DURATION_CONFIG.get();
+    	
+    	CURSE_BASE_CHANCE = CURSE_BASE_CHANCE_CONFIG.get();
+    	CURSE_BASE_DURATION = CURSE_BASE_DURATION_CONFIG.get();
+    	
+    	HARM_BASE_CHANCE = HARM_BASE_CHANCE_CONFIG.get();
+    	HARM_BASE_DURATION = HARM_BASE_DURATION_CONFIG.get();
+    	
+    	LEVITATE_BASE_CHANCE = LEVITATE_BASE_CHANCE_CONFIG.get();
+    	LEVITATE_BASE_DURATION = LEVITATE_BASE_DURATION_CONFIG.get();
+    	
+    	LIGHTNING_STRIKER_BASE_CHANCE = LIGHTNING_STRIKER_BASE_CHANCE_CONFIG.get();
+    	LIGHTNING_STRIKER_BASE_DURATION = LIGHTNING_STRIKER_BASE_DURATION_CONFIG.get();
+    	
+    	SLOW_BASE_CHANCE = SLOW_BASE_CHANCE_CONFIG.get();
+    	SLOW_BASE_DURATION = SLOW_BASE_DURATION_CONFIG.get();
+    	
+    	STARVE_BASE_CHANCE = STARVE_BASE_CHANCE_CONFIG.get();
+    	STARVE_BASE_DURATION = STARVE_BASE_DURATION_CONFIG.get();
+    	
+    	TEMPO_THEFT_BASE_CHANCE = TEMPO_THEFT_BASE_CHANCE_CONFIG.get();
+    	TEMPO_THEFT_BASE_DURATION = TEMPO_THEFT_BASE_DURATION_CONFIG.get();
+    	
+    	WEAKEN_BASE_CHANCE = WEAKEN_BASE_CHANCE_CONFIG.get();
+    	WEAKEN_BASE_DURATION = WEAKEN_BASE_DURATION_CONFIG.get();
 	}
 }
