cb6f62fd1dfc8f42f5d597e5f06b4104e048e63e
Admir Tuzović <admir.tuzovic@appimpact.ba>
2024-02-18 23:15:08 +0100

    Moved rampaging lock to be attribute modifier.

--- src/main/java/ba/minecraft/uniquemagic/common/mobeffects/beneficial/RampagingMobEffect.java
+++ src/main/java/ba/minecraft/uniquemagic/common/mobeffects/beneficial/RampagingMobEffect.java
@@ -1,49 +1,16 @@
 package ba.minecraft.uniquemagic.common.mobeffects.beneficial;
 
-import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.ai.attributes.AttributeInstance;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier.Operation;
 import net.minecraft.world.entity.ai.attributes.Attributes;
-import net.minecraft.world.entity.player.Player;
 
-public class RampagingMobEffect extends MobEffect{
+public final class RampagingMobEffect extends MobEffect {
 	
 	 public RampagingMobEffect() {
         super(MobEffectCategory.BENEFICIAL, 0xFFFFFF); // Change the color to what you prefer
+        
+		this.addAttributeModifier(Attributes.ATTACK_SPEED, "1B4A6E69-1E81-4854-A36A-8CADE80AAD45", 0.5, Operation.MULTIPLY_TOTAL);
     }
 
-	@Override
-	public void applyEffectTick(LivingEntity livingEntity, int amplifier) {
-		
-		// IF: Code is executing on client side.
-		if(livingEntity.level().isClientSide()) {
-			
-			// Do nothing.
-			return;
-		}
-		
-		// IF: Entity is not player.
-		if(!(livingEntity instanceof Player)) {
-			
-			// Do nothing.
-			return;
-		}
-		
-		// Cast living entity to server player.
-		ServerPlayer player = (ServerPlayer)livingEntity;
-		
-		// Get reference to attack speed attribute.
-		AttributeInstance attribute = player.getAttribute(Attributes.ATTACK_SPEED);
-		
-		attribute.setBaseValue(4 * (1.5 + amplifier * 0.5)); // 50% increase plus 50% per amplifier level } }
-	}
-
-	@Override
-	public boolean shouldApplyEffectTickThisTick(int pDuration, int pAmplifier) {
-		// Ensures that effect is applied for every tick.
-		return true;
-	}
-	 
 }
