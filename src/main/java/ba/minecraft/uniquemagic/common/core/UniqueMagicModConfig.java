6f817836f09ab5c7b4c4aee259770510fb53fca2
Admir Tuzović <admir.tuzovic@appimpact.ba>
2024-05-19 22:06:56 +0200

    Added lightning protection enchantment.

--- src/main/java/ba/minecraft/uniquemagic/common/core/UniqueMagicModConfig.java
+++ src/main/java/ba/minecraft/uniquemagic/common/core/UniqueMagicModConfig.java
@@ -57,11 +57,12 @@ public final class UniqueMagicModConfig {
     private static final ForgeConfigSpec.IntValue DISARM_BASE_CHANCE_CONFIG;
     
     private static final ForgeConfigSpec.IntValue ILLAGERS_BANE_BASE_MULTIPLIER_CONFIG;
+    private static final ForgeConfigSpec.IntValue NETHER_SLAYER_BASE_MULTIPLIER_CONFIG;
     
     private static final ForgeConfigSpec.IntValue RAMPAGE_BASE_CHANCE_CONFIG;
     private static final ForgeConfigSpec.IntValue RAMPAGE_BASE_DURATION_CONFIG;
     
-    private static final ForgeConfigSpec.IntValue NETHER_SLAYER_BASE_MULTIPLIER_CONFIG;
+    private static final ForgeConfigSpec.IntValue LIGHTNING_PROTECTION_BASE_MULTIPLIER_CONFIG;
     
     public static int EXECUTE_BASE_CHANCE;
     public static int LIFE_STEAL_BASE_RATIO;
@@ -108,11 +109,13 @@ public final class UniqueMagicModConfig {
     public static int DISARM_BASE_CHANCE;
     
     public static int ILLAGERS_BANE_BASE_MULTIPLIER;
-    
+    public static int NETHER_SLAYER_BASE_MULTIPLIER;
+
+    public static int LIGHTNING_PROTECTION_BASE_MULTIPLIER;
+
     public static int RAMPAGE_BASE_CHANCE;
     public static int RAMPAGE_BASE_DURATION;
     
-    public static int NETHER_SLAYER_BASE_MULTIPLIER;
     static {
     	BUILDER.push("Configs for Unique Magic Mod");
 
@@ -205,15 +208,18 @@ public final class UniqueMagicModConfig {
     	
     	ILLAGERS_BANE_BASE_MULTIPLIER_CONFIG = BUILDER.comment("Base multiplier for Illager's Bane enchantment.")
     	        .defineInRange("Illager's Bane base multiplier", 3, 1, 10);
-    	
+
+    	NETHER_SLAYER_BASE_MULTIPLIER_CONFIG = BUILDER.comment("Base multiplier for Nether Slayer enchantment.")
+    	        .defineInRange("Nether Slayer base multiplier", 3, 1, 10);
+
     	RAMPAGE_BASE_CHANCE_CONFIG = BUILDER.comment("Base chance for Rampage enchantment to trigger.")
     	        .defineInRange("Rampage base chance", 5, 1, 20);
 
     	RAMPAGE_BASE_DURATION_CONFIG = BUILDER.comment("Base duration for Rampage enchantment in seconds.")
     	        .defineInRange("Rampage base duration", 3, 1, 60);
-    	
-    	NETHER_SLAYER_BASE_MULTIPLIER_CONFIG = BUILDER.comment("Base multiplier for Nether Slayer enchantment.")
-    	        .defineInRange("Nether Slayer base multiplier", 3, 1, 10);
+
+    	LIGHTNING_PROTECTION_BASE_MULTIPLIER_CONFIG = BUILDER.comment("Base multiplier for Lightning Protection enchantment.")
+    	        .defineInRange("Lightning Protection base multiplier", 2, 1, 3);
 
     	BUILDER.pop();
     	
@@ -269,10 +275,11 @@ public final class UniqueMagicModConfig {
     	DISARM_BASE_CHANCE = DISARM_BASE_CHANCE_CONFIG.get();
     	
     	ILLAGERS_BANE_BASE_MULTIPLIER = ILLAGERS_BANE_BASE_MULTIPLIER_CONFIG.get();
+    	NETHER_SLAYER_BASE_MULTIPLIER = NETHER_SLAYER_BASE_MULTIPLIER_CONFIG.get();
 	
      	RAMPAGE_BASE_CHANCE = RAMPAGE_BASE_CHANCE_CONFIG.get();
     	RAMPAGE_BASE_DURATION = RAMPAGE_BASE_DURATION_CONFIG.get();
-    	
-    	NETHER_SLAYER_BASE_MULTIPLIER = NETHER_SLAYER_BASE_MULTIPLIER_CONFIG.get();
+
+    	LIGHTNING_PROTECTION_BASE_MULTIPLIER = LIGHTNING_PROTECTION_BASE_MULTIPLIER_CONFIG.get();
     }
 }
--- src/main/java/ba/minecraft/uniquemagic/common/enchantments/ArmorEnchants.java
+++ src/main/java/ba/minecraft/uniquemagic/common/enchantments/ArmorEnchants.java
@@ -1,6 +1,7 @@
 package ba.minecraft.uniquemagic.common.enchantments;
 
 import ba.minecraft.uniquemagic.common.core.UniqueMagicMod;
+import ba.minecraft.uniquemagic.common.enchantments.armor.LightningProtectionEnchantment;
 import ba.minecraft.uniquemagic.common.enchantments.armor.OmnivoreEnchantment;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraftforge.registries.DeferredRegister;
@@ -14,4 +15,7 @@ public class ArmorEnchants {
 	
 	public static final RegistryObject<Enchantment> OMNIVORE = 
 			REGISTRY.register("omnivore",  () -> new OmnivoreEnchantment());
+
+	public static final RegistryObject<Enchantment> LIGHTNING_PROTECTION = 
+			REGISTRY.register("lightning_protection",  () -> new LightningProtectionEnchantment());
 }
