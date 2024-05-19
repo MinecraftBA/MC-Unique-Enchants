1f35ff25f52a7b6073acd1795ed94ad886c16eaa
AzureAD\DilanTuzovic <dilan.tuzovic@appimpact.ba>
2024-05-19 20:37:31 +0200

    Fixed imports

--- src/main/java/ba/minecraft/uniquemagic/common/enchantments/weapon/NetherSlayerEnchantment.java
+++ src/main/java/ba/minecraft/uniquemagic/common/enchantments/weapon/NetherSlayerEnchantment.java
@@ -9,7 +9,6 @@ import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.Enchantment;
-import net.minecraft.world.item.enchantment.EnchantmentCategory;
 
 public class NetherSlayerEnchantment  extends ModEnchantment {
 
