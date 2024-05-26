67233741a5ceca3516b68a853e24f90b320ac1ae
Admir Tuzović <admir.tuzovic@appimpact.ba>
2024-05-26 18:36:36 +0200

    Corrections on exploration enchantment.

--- src/main/java/ba/minecraft/uniquemagic/common/event/enchantments/armor/ExplorationEnchantmentEventHandler.java
+++ src/main/java/ba/minecraft/uniquemagic/common/event/enchantments/armor/ExplorationEnchantmentEventHandler.java
@@ -1,11 +1,13 @@
 package ba.minecraft.uniquemagic.common.event.enchantments.armor;
 
 import ba.minecraft.uniquemagic.common.core.UniqueMagicMod;
+import ba.minecraft.uniquemagic.common.core.UniqueMagicModConfig;
 import ba.minecraft.uniquemagic.common.enchantments.ArmorEnchants;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import net.minecraft.world.level.Level;
 import net.minecraftforge.event.entity.player.PlayerXpEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
@@ -15,16 +17,46 @@ import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 public class ExplorationEnchantmentEventHandler {
 	
 	@SubscribeEvent
-	public void onPlayerGainXP(PlayerXpEvent.XpChange event) {
+	public void onPlayerXpChange(final PlayerXpEvent.XpChange event) {
+
+		// Get reference to player that has picked up orb.
         Player player = event.getEntity();
-        if (player != null) {
-            ItemStack helmet = player.getItemBySlot(EquipmentSlot.HEAD);
-            int level = EnchantmentHelper.getItemEnchantmentLevel(ArmorEnchants.EXPLORATION.get(), helmet);
-            if (level > 0) {
-                event.setAmount(event.getAmount() * (1 + level));
-            }
+        
+        // Get reference to level where code is executing.
+        Level level = player.level();
+        
+        // IF: Code is executing on client side.
+        if(level.isClientSide) {
+        	
+        	// Do nothing.
+        	return;
+        }
+
+        // Get reference to helmet that player is carrying.
+    	ItemStack helmet = player.getItemBySlot(EquipmentSlot.HEAD);
+        
+    	// IF: Player is not carrying helmet.
+    	if(helmet == null) {
+    		
+    		// Do nothing.
+    		return;
+    	}
+    	
+    	// Get enchantment level on helmet.
+    	int enchantmentLevel = EnchantmentHelper.getItemEnchantmentLevel(ArmorEnchants.EXPLORATION.get(), helmet);
+        
+    	// IF: There is
+    	if (enchantmentLevel > 0) {
+    		
+    		// Calculate increment ratio based on the confiugration and enchantment level.
+    		double incrementRatio = UniqueMagicModConfig.EXPLORATION_BASE_PERCENTAGE_INCREMENT * enchantmentLevel / (double)100;
+    		
+    		// Calculate the amount of XP after increment.
+    		int xpAmount = (int)(event.getAmount() * (1 + incrementRatio));
+    		
+    		// Set XP amount to be given to player.
+            event.setAmount(xpAmount);
         }
     }
-
 	
 }
