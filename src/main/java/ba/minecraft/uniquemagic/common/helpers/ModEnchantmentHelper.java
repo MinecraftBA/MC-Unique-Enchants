fcd55c20ee5611afe1e43099c0cc3e9c17fe7b0d
Admir Tuzović <admir.tuzovic@appimpact.ba>
2024-06-01 17:01:50 +0200

    Fixed issue with enchantment compatibility check.

--- src/main/java/ba/minecraft/uniquemagic/common/helpers/ModEnchantmentHelper.java
+++ src/main/java/ba/minecraft/uniquemagic/common/helpers/ModEnchantmentHelper.java
@@ -1,7 +1,6 @@
 package ba.minecraft.uniquemagic.common.helpers;
 
 import net.minecraft.core.Holder.Reference;
-import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.enchantment.Enchantment;
 
@@ -9,8 +8,7 @@ public final class ModEnchantmentHelper {
 
 	public static boolean is(Enchantment enchantment, TagKey<Enchantment> tag) {
 
-		// Get reference to enchantment holder.
-		Reference<Enchantment> holder = BuiltInRegistries.ENCHANTMENT.createIntrusiveHolder(enchantment);
+		Reference<Enchantment> holder = enchantment.builtInRegistryHolder();
 		
 		// Check if enchantment is tagged.
 		return holder.is(tag);
