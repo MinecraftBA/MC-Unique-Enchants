36a2252c850d2b94dd9d30e74c755204fc71fa7e
Admir Tuzović <admir.tuzovic@appimpact.ba>
2024-06-26 20:33:43 +0200

    Added missing tag for enchantments to show up in enchantment table.

--- src/main/java/ba/minecraft/uniquemagic/datagen/tag/ModEnchantmentTagsProvider.java
+++ src/main/java/ba/minecraft/uniquemagic/datagen/tag/ModEnchantmentTagsProvider.java
@@ -3,6 +3,8 @@ package ba.minecraft.uniquemagic.datagen.tag;
 import java.util.concurrent.CompletableFuture;
 
 import ba.minecraft.uniquemagic.common.enchantments.ArmorEnchantments;
+import ba.minecraft.uniquemagic.common.enchantments.BowEnchantments;
+import ba.minecraft.uniquemagic.common.enchantments.ToolEnchantments;
 import ba.minecraft.uniquemagic.common.enchantments.WeaponEnchantments;
 import ba.minecraft.uniquemagic.common.tags.ModEnchantmentTags;
 import net.minecraft.core.HolderLookup.Provider;
@@ -20,6 +22,40 @@ public final class ModEnchantmentTagsProvider extends EnchantmentTagsProvider {
 	@Override
 	protected void addTags(Provider provider) {
 
+		// Add all mod specific enchantments here.
+	    tag(ModEnchantmentTags.UNIQUE_MAGIC)
+	    	.add(ArmorEnchantments.EXPLORATION)
+	        .add(ArmorEnchantments.FREEZING_PROTECTION)
+	        .add(ArmorEnchantments.LIGHTNING_PROTECTION)
+	        .add(ArmorEnchantments.MAGIC_PROTECTION)
+	        .add(ArmorEnchantments.OMNIVORE)
+	        .add(ArmorEnchantments.SONIC_PROTECTION)
+	        .add(BowEnchantments.TRIGGER)
+	        .add(ToolEnchantments.TIMBER)
+	        .add(WeaponEnchantments.BLIND)
+	        .add(WeaponEnchantments.CONFUSE)
+	        .add(WeaponEnchantments.CURSE_OF_SLOWNESS)
+	        .add(WeaponEnchantments.DISARM)
+	        .add(WeaponEnchantments.EXECUTE)
+	        .add(WeaponEnchantments.HARM)
+	        .add(WeaponEnchantments.ILLAGERS_BANE)
+	        .add(WeaponEnchantments.LEVITATE)
+	        .add(WeaponEnchantments.LIFE_STEAL)
+	        .add(WeaponEnchantments.NETHER_SLAYER)
+	        .add(WeaponEnchantments.PILLAGING)
+	        .add(WeaponEnchantments.POISON)
+	        .add(WeaponEnchantments.RAMPAGE)
+	        .add(WeaponEnchantments.SLOW)
+	        .add(WeaponEnchantments.STARVE)
+	        .add(WeaponEnchantments.STUN)
+	        .add(WeaponEnchantments.TEMPO_THEFT)
+	        .add(WeaponEnchantments.THUNDERING)
+	        .add(WeaponEnchantments.WEAKEN)
+	        .add(WeaponEnchantments.WITHERING);
+		
+	    tag(EnchantmentTags.IN_ENCHANTING_TABLE)
+	    	.addTag(ModEnchantmentTags.UNIQUE_MAGIC);
+	    
 		tag(EnchantmentTags.ARMOR_EXCLUSIVE)
         	.add(ArmorEnchantments.FREEZING_PROTECTION)
         	.add(ArmorEnchantments.LIGHTNING_PROTECTION)
